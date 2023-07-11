0e0030befe6862728dc3c924e4c8c1f5f4b8c486
Neoklis Varsamos <neoklis.varsamos@peoplecert.org>
2023-07-11 11:27:41 +0300

    charts are ready

--- Frontend/expense/src/views/Charts/Bars.vue
+++ Frontend/expense/src/views/Charts/Bars.vue
@@ -1,9 +1,92 @@
 <script setup>
-    import chart from '@/components/Chart.vue'
+import { onMounted, ref, watch } from 'vue';
+import axios from 'axios';
+import Chart from 'chart.js/auto';
+
+const labels = ref([]);
+const amount = ref([]);
+
+const data = {
+    labels: labels.value,
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+};
+
+onMounted(async () => {
+    try {
+        // Fetch the chart data from the API
+        const response = await axios.get(
+            'https://localhost:7235/api/ChartData/GetAll'
+        );
+        const chartData = response.data;
+        console.log(chartData);
+
+        // Extract labels and amounts from the chart data
+        labels.value = chartData.map(item => item.category);
+        amount.value = chartData.map(item => item.amountSpend);
+        console.log(labels.value);
+        console.log(amount.value);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+watch([labels, amount], ([newLabels, newAmount]) => {
+    const newDatasets = [];
+
+    for (let i = 0; i < newAmount.length; i++) {
+        // Create datasets with respective labels and amounts
+        newDatasets.push({
+            label: newLabels[i],
+            data: [newAmount[i]],
+            backgroundColor: generateRandomColors(newAmount.length, 1),
+            borderColor: (0, 191, 255, 1),
+            borderWidth: 1,
+        });
+        console.log(newAmount[i])
+    }
+
+    // Update the chart data with new labels and datasets
+    data.labels = ' ';
+    data.datasets = newDatasets;
+
+    // Create the chart using the updated data and config
+    const myChart = new Chart(document.getElementById('myChart'), config);
+});
+
+// Helper function to generate an array of random colors
+function generateRandomColors(count, opacity) {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+        const color = `rgba(${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, ${opacity})`;
+        colors.push(color);
+    }
+    return colors;
+}
+
+// Helper function to generate a random value within a range
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 </script>
 
-
-
 <template>
-    <chart></chart>
+    <div class="container">
+        <div class="card text-center">
+            <div class="card-header">Expense per Category</div>
+            <div class="card-body">
+                <canvas id="myChart" class="chart-canvas"></canvas>
+            </div>
+        </div>
+    </div>
 </template>
--- Frontend/expense/src/views/Charts/Pie.vue
+++ Frontend/expense/src/views/Charts/Pie.vue
@@ -1,10 +1,87 @@
 <script setup>
-import chart from '@/components/Chart.vue';
+import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
+import Chart from 'chart.js/auto';
+
+const labels = ref([]);
+const amount = ref([]);
+
+const data = {
+    labels: labels.value,
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+        },
+        layout: {
+            padding: {
+                left: 50,
+                right: 50,
+                top: 0,
+                bottom: 0,
+            },
+        },
+        maintainAspectRatio: false,
+    },
+};
+
+onMounted(async () => {
+    try {
+        // Fetch the chart data from the API
+        const response = await axios.get('https://localhost:7235/api/ChartData/GetAll');
+        const chartData = response.data;
+        labels.value = chartData.map(item => item.category);
+        amount.value = chartData.map(item => item.amountSpend);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+watch([labels, amount], ([newLabels, newAmount]) => {
+    const newDatasets = [{
+        data: newAmount,
+        backgroundColor: generateRandomColors(newAmount.length, 1),
+        borderColor: (0,191,255, 1),
+        borderWidth: 1,
+    }];
+
+    data.labels = newLabels;
+    data.datasets = newDatasets;
+
+    const myChart = new Chart(document.getElementById('myChart'), config);
+});
+
+// Helper function to generate an array of random colors
+function generateRandomColors(count, opacity) {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+        const color = `rgba(${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, ${opacity})`;
+        colors.push(color);
+        console.log(color)
+    }
+    return colors;
+}
+
+// Helper function to generate a random value within a range
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 </script>
 
-
-
 <template>
-    <chart></chart>
+    <div class="container">
+        <div class="card text-center">
+            <div class="card-header">Expense per Category</div>
+            <div class="card-body">
+                <canvas id="myChart" class="chart-canvas"></canvas>
+            </div>
+        </div>
+    </div>
 </template>
